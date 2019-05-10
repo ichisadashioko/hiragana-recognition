ba7a245e5ae886017f125fdc002110b4c0e897ef
kuroemon2509 <yokobang99@gmail.com>
2019-05-10 23:49:06 +0700

    bug fix

--- drawing.js
+++ drawing.js
@@ -83,10 +83,6 @@ function clearCanvas() {
     ctx.fillRect(0, 0, width, height);
 }
 
-function predictCanvas() {
-
-}
-
 /**
  * Initialize pen
  * @param {Touch} touch 
@@ -153,105 +149,4 @@ function touchend(touch) {
             pen.y = null
         }
     }
-}
-
-/**
- * 
- * @param {MouseEvent} mouse 
- */
-function mouse_down(mouse) {
-    pen.down = true
-}
-/**
- * 
- * @param {MouseEvent} mouse 
- */
-function mouse_move(mouse) {
-    if (pen.down) {
-        if (pen.x == null || pen.y == null) {
-            pen.x = (mouse.clientX - canvasOffset.x) / scale
-            pen.y = (mouse.clientY - canvasOffset.y) / scale
-        } else {
-            var x = pen.x
-            var y = pen.y
-
-            var dX = (mouse.clientX - canvasOffset.x) / scale - x;
-            var dY = (mouse.clientY - canvasOffset.y) / scale - y;
-
-            var distance = Math.sqrt(dX ** 2 + dY ** 2)
-
-            var step = distance / MIN_DISTANCE
-            var deltaX = dX / step
-            var deltaY = dY / step
-
-            for (let i = 0; i < step; i++) {
-                ctx.fillStyle = brushStyle;
-                ctx.beginPath()
-                ctx.arc(x, y, pen.radius, 0, Math.PI * 2, false)
-                ctx.fill()
-
-                x += deltaX
-                y += deltaY
-            }
-            pen.x = (mouse.clientX - canvasOffset.x) / scale
-            pen.y = (mouse.clientY - canvasOffset.y) / scale
-
-            isBlank = false
-        }
-    }
-}
-/**
- * 
- * @param {MouseEvent} mouse 
- */
-function mouse_up(mouse) {
-    pen.down = false
-    pen.x = null
-    pen.y = null
-    predict()
-}
-window.onload = function () {
-
-    canvas = document.getElementById('canvas');
-    ctx = canvas.getContext('2d');
-
-    doResize();
-    clearCanvas();
-
-    canvas.addEventListener('touchstart', (evt) => {
-        evt.preventDefault()
-        for (var i = 0; i < evt.changedTouches.length; i++) {
-            touchstart(evt.changedTouches[i])
-        }
-    }, false)
-
-    canvas.addEventListener('touchmove', (evt) => {
-        evt.preventDefault()
-        for (var i = 0; i < evt.changedTouches.length; i++) {
-            touchmove(evt.changedTouches[i])
-        }
-    }, false)
-
-    canvas.addEventListener('touchend', (evt) => {
-        evt.preventDefault()
-        for (var i = 0; i < evt.changedTouches.length; i++) {
-            touchend(evt.changedTouches[i])
-        }
-    }, false)
-
-    canvas.addEventListener('mousedown', (evt) => mouse_down(evt))
-    canvas.addEventListener('mousemove', (evt) => mouse_move(evt))
-    canvas.addEventListener('mouseup', (evt) => mouse_up(evt))
-    canvas.addEventListener('mouseleave', (evt) => mouse_up(evt))
-
-    window.addEventListener('keyup', (evt) => {
-        // console.log(evt)
-        if (evt.key == 'c') {
-            clearCanvas()
-        }
-    })
-
-    window.addEventListener('resize', function (e) {
-        doResize();
-    })
 }
--- main.js
+++ main.js
@@ -10,17 +10,6 @@ async function loadMyModel() {
 
 loadMyModel()
 
-
-function pushResult(mapped_result) {
-    template = $('#result-template').html()
-
-
-    // $('#result-container').append(
-    //     $('<div/>').attr('role', 'alert').addClass('toast')
-    //     .append('<')
-    // )
-}
-
 function predict() {
     var canvas = document.getElementById('canvas')
     input_image = tf.browser.fromPixels(canvas, 1)
@@ -54,5 +43,107 @@ function predict() {
 
         // template = $('#result-template').html().replace('result_message_here', toast_message)
         $('#result-container').html(template)
+    })
+}
+
+/**
+ * 
+ * @param {MouseEvent} mouse 
+ */
+function mouse_down(mouse) {
+    pen.down = true
+}
+/**
+ * 
+ * @param {MouseEvent} mouse 
+ */
+function mouse_move(mouse) {
+    if (pen.down) {
+        if (pen.x == null || pen.y == null) {
+            pen.x = (mouse.clientX - canvasOffset.x) / scale
+            pen.y = (mouse.clientY - canvasOffset.y) / scale
+        } else {
+            var x = pen.x
+            var y = pen.y
+
+            var dX = (mouse.clientX - canvasOffset.x) / scale - x;
+            var dY = (mouse.clientY - canvasOffset.y) / scale - y;
+
+            var distance = Math.sqrt(dX ** 2 + dY ** 2)
+
+            var step = distance / MIN_DISTANCE
+            var deltaX = dX / step
+            var deltaY = dY / step
+
+            for (let i = 0; i < step; i++) {
+                ctx.fillStyle = brushStyle;
+                ctx.beginPath()
+                ctx.arc(x, y, pen.radius, 0, Math.PI * 2, false)
+                ctx.fill()
+
+                x += deltaX
+                y += deltaY
+            }
+            pen.x = (mouse.clientX - canvasOffset.x) / scale
+            pen.y = (mouse.clientY - canvasOffset.y) / scale
+
+            isBlank = false
+        }
+    }
+}
+
+/**
+ * 
+ * @param {MouseEvent} mouse 
+ */
+function mouse_up(mouse) {
+    pen.down = false
+    pen.x = null
+    pen.y = null
+    predict()
+}
+
+window.onload = function () {
+
+    canvas = document.getElementById('canvas');
+    ctx = canvas.getContext('2d');
+
+    doResize();
+    clearCanvas();
+
+    canvas.addEventListener('touchstart', (evt) => {
+        evt.preventDefault()
+        for (var i = 0; i < evt.changedTouches.length; i++) {
+            touchstart(evt.changedTouches[i])
+        }
+    }, false)
+
+    canvas.addEventListener('touchmove', (evt) => {
+        evt.preventDefault()
+        for (var i = 0; i < evt.changedTouches.length; i++) {
+            touchmove(evt.changedTouches[i])
+        }
+    }, false)
+
+    canvas.addEventListener('touchend', (evt) => {
+        evt.preventDefault()
+        for (var i = 0; i < evt.changedTouches.length; i++) {
+            touchend(evt.changedTouches[i])
+        }
+    }, false)
+
+    canvas.addEventListener('mousedown', (evt) => mouse_down(evt))
+    canvas.addEventListener('mousemove', (evt) => mouse_move(evt))
+    canvas.addEventListener('mouseup', (evt) => mouse_up(evt))
+    canvas.addEventListener('mouseleave', (evt) => mouse_up(evt))
+
+    window.addEventListener('keyup', (evt) => {
+        if (evt.key == 'c') {
+            clearCanvas()
+        }
+    })
+
+    window.addEventListener('resize', function (e) {
+        doResize();
     })
 }
