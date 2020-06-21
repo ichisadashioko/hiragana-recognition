349376f639cb3eb83251edf0429855e0694c0de5
Ichisada Shioko <ichisadashioko@gmail.com>
2020-06-21 19:59:18 +0900

    :construction: migrating serializing format

--- serializable.py
+++ serializable.py
@@ -1,9 +1,12 @@
+import os
+import io
 import json
 from typing import List, Dict
 
 
 def universal_dump(obj, fp):
-    json.dump(obj, fp, ensure_ascii=False, indent=4)
+    # use tab to reduce file size
+    json.dump(obj, fp, ensure_ascii=False, indent='\t')
 
 
 class LabelFile:
@@ -11,6 +14,9 @@ class LabelFile:
         self.source = source
         self.content = content
         self.labels = labels
+
+    def __repr__(self):
+        return repr(self.__dict__)
 
     @staticmethod
     def parse_obj(obj: dict):
@@ -115,3 +121,170 @@ class DatasetMetadata:
                 args[key] = obj[key]
 
         return DatasetMetadata(**args)
+
+
+# New data serialization format (because other formats are terrible)
+# JSON - cannot be appended on the fly
+# YAML - specification is too large
+# TFRecord/protobuf - I cannot find where is the specification. They
+# just tell me how great the technology is and there are only samples on
+# using TFRecord with TensorFlow and it is terribly slow. I want to be
+# able to use pure Python without TensorFlow. In addition, the whole
+# TensorFlow graph thing makes it imposible to debug. I think they
+# JIT compile my code for using with CUDA or native DLLs.
+# I'm not gonna live with those specification limitations!
+
+# It's not going to be human readable because we need to store binary
+# data.
+
+# - Format: data length in bytes and followed by the data
+# - There will be only list and records will be stacked one after other
+# - To present a dictionary: Write the key and then the value, key and
+# value are treated as 2 records in list. It's user's job to most of
+# the parsing and converting to required data type.
+# - If the data length is 0 but there is still data left then they are
+# treated as metadata. You will need to ship your parsing and converting
+# code for the blob.
+
+# number of bytes to store the record length is 4 bytes (32 bits)
+class XFormat:
+    INT_SIZE = 4
+    BYTE_ORDER = 'little'
+    EXTENSION = '.xformat'
+    ENCODING = 'utf-8'
+
+    DATA_TYPE_BYTES = 0
+    DATA_TYPE_INT = 1
+    DATA_TYPE_UTF8_STRING = 2
+    DATA_TYPE_LIST = 3
+    DATA_TYPE_DICT = 4
+
+    @classmethod
+    def serialize_string(cls, s: str) -> bytes:
+        record_data = s.encode(encoding=cls.ENCODING)
+        return record_data
+
+    @classmethod
+    def deserialize_string(cls, bs: bytes) -> str:
+        return bs.decode(encoding=cls.ENCODING)
+
+    @classmethod
+    def serialize_int(cls, n: int) -> bytes:
+        record_data = n.to_bytes(
+            length=cls.INT_SIZE,
+            byteorder=cls.BYTE_ORDER,
+            signed=True,
+        )
+
+        return record_data
+
+    @classmethod
+    def deserialize_int(cls, bs: bytes) -> int:
+        return int.from_bytes(bs, byteorder=cls.BYTE_ORDER, signed=True)
+
+    @classmethod
+    def serialize_obj(cls, obj) -> (bytes, bytes):
+        obj_type = type(obj)
+        if obj_type == int:
+            return bytes([cls.DATA_TYPE_INT]), cls.serialize_int(obj)
+        elif obj_type == str:
+            return bytes([cls.DATA_TYPE_UTF8_STRING]), cls.serialize_string(obj)
+        elif obj_type == bytes:
+            return bytes([cls.DATA_TYPE_BYTES]), obj
+        elif obj_type == list:
+            buffer = io.BytesIO()
+
+            for value in obj:
+                datatype, encoded_value = cls.serialize_obj(value)
+                buffer.write(datatype)
+                buffer.write(cls.serialize_int(len(encoded_value)))
+                buffer.write(encoded_value)
+
+            return bytes([cls.DATA_TYPE_LIST]), buffer.getvalue()
+        elif obj_type == dict:
+            buffer = io.BytesIO()
+
+            for key in obj:
+                datatype, encoded_key = cls.serialize_obj(key)
+                buffer.write(datatype)
+                buffer.write(cls.serialize_int(len(encoded_key)))
+                buffer.write(encoded_key)
+
+                datatype, encoded_value = cls.serialize_obj(obj[key])
+                buffer.write(datatype)
+                buffer.write(cls.serialize_int(len(encoded_value)))
+                buffer.write(encoded_value)
+
+            return bytes([cls.DATA_TYPE_DICT]), buffer.getvalue()
+        else:
+            raise Exception(f'Unsupported type {obj_type}!')
+            return 0
+
+    @classmethod
+    def deserialze_obj(cls, bs: bytes, datatype: int):
+        if datatype == cls.DATA_TYPE_BYTES:
+            return bs
+        elif datatype == cls.DATA_TYPE_INT:
+            return cls.deserialize_int(bs)
+        elif datatype == cls.DATA_TYPE_UTF8_STRING:
+            return cls.deserialize_string(bs)
+        elif datatype == cls.DATA_TYPE_LIST:
+            retval = []
+            buffer = io.BytesIO(bs)
+            pos = 0
+            bs_len = len(bs)
+
+            while pos < bs_len:
+                value_datatype = bs[pos]
+                pos += 1
+
+                if(pos + cls.INT_SIZE) > bs_len:
+                    raise Exception(f'Broken serialized data!')
+                value_byte_count = cls.deserialize_int(bs[pos:pos+cls.INT_SIZE])  # noqa
+                pos += cls.INT_SIZE
+
+                if(pos + value_byte_count) > bs_len:
+                    raise Exception(f'Broken serialized data!')
+                value = cls.deserialze_obj(bs[pos:pos+value_byte_count], value_datatype)  # noqa
+                retval.append(value)
+                pos += value_byte_count
+
+            return retval
+        elif datatype == cls.DATA_TYPE_DICT:
+            retval = {}
+            buffer = io.BytesIO(bs)
+            pos = 0
+            bs_len = len(bs)
+
+            while pos < bs_len:
+                key_datatype = bs[pos]
+                pos += 1
+
+                if (pos + cls.INT_SIZE) > bs_len:
+                    raise Exception(f'Broken serialized data!')
+                key_byte_count = cls.deserialize_int(bs[pos:pos+cls.INT_SIZE])
+                pos += cls.INT_SIZE
+
+                if(pos + key_byte_count) > bs_len:
+                    raise Exception(f'Broken serialized data!')
+                key = cls.deserialze_obj(bs[pos:pos+key_byte_count], key_datatype)  # noqa
+                pos += key_byte_count
+
+                value_datatype = bs[pos]
+                pos += 1
+
+                if(pos + cls.INT_SIZE) > bs_len:
+                    raise Exception(f'Broken serialized data!')
+                value_byte_count = cls.deserialize_int(bs[pos:pos+cls.INT_SIZE])  # noqa
+                pos += cls.INT_SIZE
+
+                if(pos + value_byte_count) > bs_len:
+                    raise Exception(f'Broken serialized data!')
+                value = cls.deserialze_obj(bs[pos:pos+value_byte_count], value_datatype)  # noqa
+                pos += value_byte_count
+
+                retval[key] = value
+
+            return retval
+        else:
+            raise Exception(f'Unsupported data type {datatype}!')
