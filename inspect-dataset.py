54a99339623d492132d87b4783e6dfa4f7125f64
Ichisada Shioko <ichisadashioko@gmail.com>
2020-08-29 19:24:55 +0900

    create hello world gui with tkinter

---
+++ inspect-dataset.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# encoding=utf-8
+import os
+
+import tkinter as tk
+
+def main():
+    app = tk.Tk()
+    app.title('Hello World')
+    app.mainloop()
+
+if __name__ == '__main__':
+    main()
